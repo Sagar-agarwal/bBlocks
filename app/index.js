7d7bea53c7559b9ab64e1110c8843ddfaf0304f0
agarwal-sagar <agarwal.sagar@outlook.com>
2021-11-26 02:27:06 +0530

    Mine API Implemented

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const Blockchain = require("../blockchain");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
-const app = express();
 const bc = new Blockchain();
+
+const app = express();
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
 	res.json("b_blocks here !!");
@@ -12,6 +15,13 @@ app.get("/", (req, res) => {
 
 app.get("/blocks", (req, res) => {
 	res.json(bc.chain);
+});
+
+app.post("/mine", (req, res) => {
+	const block = bc.addBlock(req.body.data);
+	console.log(`New Block added: ${block.toString()}`);
+
+	res.redirect("/blocks");
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
