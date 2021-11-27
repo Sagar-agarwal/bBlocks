89b77eac9d1ed8cf75e29673b52e3d640fed6868
agarwal-sagar <agarwal.sagar@outlook.com>
2021-11-28 01:38:29 +0530

    Update transaction feature added

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -39,6 +39,21 @@ class Transaction {
 		return transaction;
 	}
 
+	updateTransaction(senderWallet, recipient, amount) {
+		const senderOutput = this.outputs.find((output) => output.address === senderWallet.publicKey);
+
+		if (amount > senderOutput.amount) {
+			console.log(`Amount ${amount} is more than remaining balance ${senderOutput.amount}`);
+			return;
+		}
+
+		senderOutput.amount = senderOutput.amount - amount;
+		this.outputs.push({ amount, address: recipient });
+		Transaction.signTransaction(this, senderWallet);
+
+		return this;
+	}
+
 	static signTransaction(transaction, senderWallet) {
 		transaction.input = {
 			timestamp: Date.now(),
